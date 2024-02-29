37d0dfa02d2cf29f6655c54ab520f4c03b9acff9
omkar <omkarbokil22@gmail.com>
2024-02-29 21:50:47 +0530

    commit 29.02.2024 by omkar

--- JS/OneShop/oneshop.js
+++ JS/OneShop/oneshop.js
@@ -1,28 +1,8 @@
 let itemOne = document.querySelectorAll(".item-1");
 let itemCard1 = document.querySelectorAll(".item-card-1");
 let buyNow = document.querySelectorAll(".buy-now");
-
-// itemOne.addEventListener("mouseover", () => {
-//      itemCard1.classList.remove("hidden");
-// })
-// itemOne.addEventListener("mouseout", () => {
-//      itemCard1.classList.add("hidden");
-// })
-// itemCard1.addEventListener("mouseover", () => {
-//      itemCard1.classList.remove("hidden");
-// })
-
-// itemOne.forEach((val, ind) => {
-//      val.addEventListener("mouseover", () => {
-//           itemCard1[ind].classList.remove("hidden");
-//      })
-// })
-
-// itemOne.forEach((val, ind) => {
-//      val.addEventListener("mouseout", () => {
-//           itemCard1[ind].classList.add("hidden");
-//      })
-// })
+let offer = document.querySelectorAll(".offer");
+let offer1 = document.querySelectorAll(".offer1");
 
 itemOne.forEach((val, ind) => {
      val.addEventListener("mouseenter", () => {
@@ -36,11 +16,14 @@ itemOne.forEach((val, ind) => {
      })
 })
 
-// itemOne.forEach((ach) => {
-//      ach.addEventListener("mouseover", () => {
-//           itemCard1.forEach((element, val) => {
-//                itemCard1[val].classList.remove("hidden");
-//                console.log("inner", val);
-//           });
-//      })
-// })
+offer.forEach((val, ind) => {
+     val.addEventListener("mouseenter", () => {
+          offer1[ind].classList.remove("hidden");
+     })
+})
+
+offer.forEach((val, ind) => {
+     val.addEventListener("mouseleave", () => {
+          offer1[ind].classList.add("hidden");
+     })
+})
