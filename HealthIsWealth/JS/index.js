355f52848e4c0a2381b1dab43a604a7ae006e41d
Omkar <omkarbokil22@gmail.com>
2024-03-08 00:52:11 +0530

    commit on 08.03.2024

---
+++ HealthIsWealth/JS/index.js
@@ -0,0 +1,14 @@
+let vegetableCard = document.querySelectorAll(".vegetable-card");
+let readInfo = document.querySelectorAll(".read-info");
+
+vegetableCard.forEach((val,ind) => {
+     val.addEventListener("mouseenter", () => {
+          readInfo[ind].classList.remove("visually-hidden")
+     })
+})
+
+vegetableCard.forEach((val,ind) => {
+     val.addEventListener("mouseleave", () => {
+          readInfo[ind].classList.add("visually-hidden")
+     })
+})
