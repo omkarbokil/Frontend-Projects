c5cb28f0c21b21df7f583d085c63deb5e4f00d48
Omkar <omkarbokil22@gmail.com>
2024-03-18 23:50:15 +0530

    ok

--- NoteIt/JS/index.js
+++ NoteIt/JS/index.js
@@ -1,86 +1,70 @@
-let parent = document.querySelector(".parent");
-let addNoteContent = document.querySelector(".add_note_content");
-let addNote = document.querySelector(".add_note");
-let okay = document.querySelector(".okay");
-let test = document.querySelector("#test");
-let noteTitle = document.querySelector("#noteTitle");
-let noteMessage = document.querySelector("#noteMessage");
-let emptyMessage = document.querySelector(".empty_message");
-let noteName = document.querySelector("#note_name");
-let notes = document.querySelectorAll(".notes")
+let parent          =    document.querySelector(".parent");
+let addNoteContent  =    document.querySelector(".add_note_content");
+let addNote         =    document.querySelector(".add_note");
+let test            =    document.querySelector("#test");
+let noteTitle       =    document.querySelector("#noteTitle");
+let noteMessage     =    document.querySelector("#noteMessage");
+let emptyMessage    =    document.querySelector(".empty_message");
+let noteName        =    document.querySelector("#note_name");
+let notes           =    document.querySelectorAll(".notes");
+// Common Variables
+let closeButton     =    document.querySelector(".close_button");
 
+// Bootstrap Tooltip JS
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+// Draggable Calculator
 //Make the DIV element draggagle:
 dragElement(document.getElementById("calculator"));
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+     if (document.getElementById(elmnt.id + "header")) {
+          /* if present, the header is where you move the DIV from:*/
+          document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+     }
+     else{
+          /* otherwise, move the DIV from anywhere inside the DIV:*/
+          elmnt.onmousedown = dragMouseDown;
+     }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+     function dragMouseDown(e) {
+          e = e || window.event;
+          e.preventDefault();
+          // get the mouse cursor position at startup:
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+          document.onmouseup = closeDragElement;
+          // call a function whenever the cursor moves:
+          document.onmousemove = elementDrag;
+     }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+     function elementDrag(e) {
+          e = e || window.event;
+          e.preventDefault();
+          // calculate the new cursor position:
+          pos1 = pos3 - e.clientX;
+          pos2 = pos4 - e.clientY;
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+          // set the element's new position:
+          elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+          elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+     }
 
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+     function closeDragElement() {
+          /* stop moving when mouse button is released:*/
+          document.onmouseup = null;
+          document.onmousemove = null;
+     }
 }
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// (() => {
-//      'use strict'
-   
-//      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//      const forms = document.querySelectorAll('.needs-validation')
-   
-//      // Loop over them and prevent submission
-//      Array.from(forms).forEach(form => {
-//        form.addEventListener('submit', event => {
-//          if (!form.checkValidity()) {
-//            event.preventDefault()
-//            event.stopPropagation()
-//          }
-   
-//          form.classList.add('was-validated')
-//        }, false)
-//      })
-//    })()
-
+// Toast Notification
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
-
-   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
+// When clicking on ADD NEW NOTE, clearing the Title and Note area, so that user can input new data
 addNote.addEventListener("click", () => {
      noteTitle.value = '';
      noteMessage.value = '';
@@ -122,10 +106,39 @@ addNoteContent.addEventListener("click", () =>{
      }
 })
 
-let a = parent.childNodes;
+// Calculator
+let calculateIcon   =    document.querySelector("#calculate_icon");
+let calculator      =    document.querySelector("#calculator");
+let calcDisp        =    document.querySelector(".calc_disp");
+let calcBtnClear    =    document.querySelector("#calc_btn_clear");
+let one             =    document.querySelector("#calc_btn_1");
+let two             =    document.querySelector("#calc_btn_2");
+let three           =    document.querySelector("#calc_btn_3");
+let four            =    document.querySelector("#calc_btn_4");
+let five            =    document.querySelector("#calc_btn_5");
+let calculatorButtons    =    document.querySelectorAll(".calculator_buttons")
 
-a.forEach((val) => {
+// When clicking on Calculator Icon Menu
+calculateIcon.addEventListener("click", ()=> {
+     calculator.classList.remove("d-none");
+})
+
+// When clikcing on Calculator close button
+closeButton.addEventListener("click", ()=> {
+     calculator.classList.add("d-none");
+})
+
+// When clicking Clear button
+calcBtnClear.addEventListener("click", ()=> {
+     calcDisp.innerText = '';
+})
+
+calculatorButtons.forEach((val) => {
      val.addEventListener("click", () => {
-          console.log(111);
+          let calcDispData = calcDisp.innerText;
+          if(val.innerText == 'C')
+               calcDisp.innerText = '';
+          else
+               calcDisp.innerText = calcDispData + val.innerText;
      })
 })
