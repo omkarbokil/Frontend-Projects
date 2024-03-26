1c06439335fe8914f3e7d9814d88ef503edd2cae
Omkar <omkarbokil22@gmail.com>
2024-03-26 22:04:04 +0530

    commmit

--- HealthIsWealth/JS/index.js
+++ HealthIsWealth/JS/index.js
@@ -1,6 +1,6 @@
 let vegetableCard = document.querySelectorAll(".vegetable-card");
 let readInfo = document.querySelectorAll(".read-info");
-let notificationBell = document.querySelector("#notification-bell")
+let notificationBell = document.querySelector("#notification-bell");
 
 vegetableCard.forEach((val,ind) => {
      val.addEventListener("mouseenter", () => {
@@ -17,3 +17,24 @@ vegetableCard.forEach((val,ind) => {
 notificationBell.addEventListener("click", () => {
      console.log('Hello');
 })
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+     const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type}2 d-flex gap-3 align-items-center" role="alert">`,
+    '<span class="material-symbols-rounded" style="color:blue">water_drop</span>',
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+].join('')
+
+alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('notification-bell')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Paani pi le bhai !!', 'primary')
+  })
+}
--- InstagramUserName/JS/index.js
+++ InstagramUserName/JS/index.js
@@ -7,15 +7,73 @@ let generateUsername = document.querySelector(".generate_btn");
 let generatedBlock  =    document.querySelector(".generated_block");
 let userInput = document.querySelector(".user_input");
 
-let usernames = ['itz','official','_','unofficial','its'];
+let usernames1 = ['@', '2024', 'official', 'king', 'social', 'i_am', 'just', 'what', 'always'];
+let usernames2 = ['@', '2024', 'official', 'king', 'social', 'i_am', 'just', 'what', 'always'];
+let usernames3 = ['@', '2024', 'official', 'king', 'social', 'i_am', 'just', 'what', 'always'];
+let usernames4 = ['@', '2024', 'official', 'king', 'social', 'i_am', 'just', 'what', 'always'];
+let checkRound = 'first';
 
 generateUsername.addEventListener("click", () => {
-     let value = userInput.value;
+     if(checkRound == 'first'){
+          usernames1.forEach((val) => {
+               kk(val);
+          })
+          checkRound = 'second';
+     }
+     else if(checkRound == 'second'){
+          usernames2.forEach((val) => {
+               kk(val);
+          })
+          checkRound = 'third';
+     }
+     else if(checkRound == 'third'){
+          usernames3.forEach((val) => {
+               kk(val);
+          })
+          checkRound = 'fourth';
+     }
+     else if(checkRound == 'fourth'){
+          usernames4.forEach((val) => {
+               kk(val);
+          })
+          checkRound = 'first';
+     }
 
-     usernames.forEach((val) => {
-          generatedBlock.innerHTML = 
-          `<div>
-               ${val}_${value}
-          </div>`;
+})
+
+kk = (val) =>{
+     let value = userInput.value;
+     let newSection = document.createElement("div");
+
+     newSection.setAttribute("class", "generated_usernames");
+     newSection.setAttribute("role", "button");
+     newSection.classList.add('px-4', 'py-1' ,'rounded-5', 'text-center');
+     if(checkRound == 'first'){
+          newSection.innerHTML = 
+               `${val}_${value}`
+               localStorage.setItem("ok",`${newSection}`);
+               console.log(localStorage.getItem("ok"));
+     }
+     else if(checkRound == 'second'){
+          newSection.innerHTML = 
+               `${val}.${value}`
+     }
+     else if(checkRound == 'third'){
+          newSection.innerHTML = 
+               `${value}_${val}`
+     }
+     else if(checkRound == 'fourth'){
+          newSection.innerHTML = 
+               `@${val}${value}`
+     }
+
+     generatedBlock.appendChild(newSection);
+}
+
+let generatedUsernames = document.querySelectorAll(".generated_usernames");
+
+(generatedBlock.childNodes).forEach((val) => {
+     val.addEventListener("click", () => {
+          console.log('no');
      })
 })
